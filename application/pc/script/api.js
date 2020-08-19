be4d27ef659a37d927c72d10cab75546f57e065b
林鸣昶 <517288724@qq.com>
2020-08-19 22:59:36 +0800

    fix

--- application/pc/script/api.js
+++ application/pc/script/api.js
@@ -1,5 +1,5 @@
 export default {
   get_item_list:"https://api.beda.one/app/resource/website_grade_list", //get 获取项目列表
   get_link_list:"https://api.beda.one/app/resource/website_link_list", //get 获取底部友联列表
-  get_grade_publicity:"/weixin/apply.php?act=get_grade_publicity", //get 获取文章宣传信息
+  get_grade_publicity:"https://api.beda.one/app/resource/grade_public_city", //get 获取文章宣传信息
 }
