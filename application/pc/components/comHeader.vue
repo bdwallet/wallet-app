babdb735952b0958a7f164c4c06858bf7d57c9fc
林鸣昶 <517288724@qq.com>
2020-11-14 20:31:49 +0800

    feat:add i18n

--- application/pc/components/comHeader.vue
+++ application/pc/components/comHeader.vue
@@ -1,31 +1,38 @@
 <template>
-	<div class="HZPJ-header">
-		<div class="container">
-			<div class="logo-box" @click="$router.push('/')">
-				<img class="logo" :src="$assets.logo" />
-				<img class="logo-text" :src="$assets.logoTextHeader" />
-			</div>
-			<ul class="tab-pc">
-				<router-link tag="li" to="/home">首页</router-link>
-				<router-link tag="li" to="/ratings">评级</router-link>
-			</ul>
-			<router-link class="tab-wap" tag="div" to="/ratings">
-				<i class="icon icon-pingji"></i>
-			</router-link>
-		</div>
-	</div>
+  <div class="HZPJ-header">
+    <div class="container">
+      <div class="logo-box" @click="$router.push('/')">
+        <img class="logo" :src="$assets[`logo`]" />
+        <img
+          class="logo-text"
+          :src="$assets[`logoTextHeader${$i18n.locale}`]"
+        />
+      </div>
+      <ul class="tab-pc">
+        <router-link tag="li" to="/home">{{ $lang("钱包") }}</router-link>
+        <router-link tag="li" to="/ratings">{{ $lang("评级") }}</router-link>
+        <li @click="handleChangeLanguage">{{ $lang("English") }}</li>
+      </ul>
+      <router-link class="tab-wap" tag="div" to="/ratings">
+        <i class="icon icon-pingji"></i>
+      </router-link>
+    </div>
+  </div>
 </template>
 
 <script>
-	const name = 'header';
-	export default {
-		name,
-		data() {
-			return {}
-		},
-		computed: {},
-		methods: {
-		},
-		mounted() {}
-	}
+const name = "header";
+export default {
+  name,
+  data() {
+    return {};
+  },
+  computed: {},
+  methods: {
+    handleChangeLanguage() {
+      this.$i18n.locale = this.$i18n.locale === "cn" ? "en" : "cn";
+    },
+  },
+  mounted() {},
+};
 </script>
--- application/pc/index/main.js
+++ application/pc/index/main.js
@@ -3,6 +3,7 @@ window.isDebug = debug;
 import babelpolyfill from 'babel-polyfill';
 import rem from '~script/rem';
 import Vue from 'vue';
+import VueI18n from 'vue-i18n';
 // 静态资源
 import Assets from '~assets';
 // 配置
@@ -24,7 +25,7 @@ Vue.prototype.$api = window.Api = Api;
 Vue.prototype.$ajax = window.Ajax = Ajax;
 Vue.prototype.$assets = window.Assets = Assets;
 
-import browser from '~script/browser';//识别设备
+import browser from '~script/browser'; //识别设备
 Vue.prototype.$browser = window.browser = browser;
 
 import FastClick from 'fastclick' //处理移动端点击300ms延迟
@@ -35,22 +36,42 @@ import routes from './routes';
 
 Vue.use(Router)
 
+Vue.use(VueI18n);
+const i18n = new VueI18n({
+	silentTranslationWarn: true,
+	locale: localStorage.getItem('locale') || 'cn',
+	fallbackLocale: 'dev',
+	messages: {
+		cn: require('./language/cn'),
+		en: require('./language/en')
+	},
+});
+
+Vue.prototype.$lang = (val, params) => {
+	const obj = params || {}; // 对象类型
+	const language = i18n.messages.cn.language;
+	const key = Object.keys(language).find(item => language[item] === val);
+	return key ? i18n.t(`language[${key}]`, obj) : val;
+}
+
 // 路由守卫
 const router = new Router({
-	linkActiveClass:'current',
-	mode: debug?'hash':'history',
+	linkActiveClass: 'current',
+	mode: debug ? 'hash' : 'history',
 	//mode: 'history',
 	routes,
-	
-	scrollBehavior (to, from, savedPosition) {
+
+	scrollBehavior(to, from, savedPosition) {
 		if (savedPosition) {
 			return savedPosition
 		} else {
-			return { x: 0, y: 0 }
+			return {
+				x: 0,
+				y: 0
+			}
 		}
 	}
 });
-
 
 router.beforeEach((to, from, next) => {
 	document.title = to.meta.title;
@@ -58,8 +79,10 @@ router.beforeEach((to, from, next) => {
 	document.description = to.meta.description;
 	next();
 });
+
 window.vm = new Vue({
 	router,
 	store,
+	i18n,
 	render: h => h(App)
 }).$mount('#app')
